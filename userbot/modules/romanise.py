a03c440f275c562151060e0cc1caf8fe0e03ecfa
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-07 17:44:33 +0530

    Update romanise.py

--- userbot/modules/romanise.py
+++ userbot/modules/romanise.py
@@ -26,7 +26,7 @@ async def _(event):
     lan = lan.strip()
 #    script = roman_gs.translate(text, lan) 
     try:
-        scripted = goslate.Goslate(text, dest=lan)
+        scripted = roman_gs.translate(text, dest=lan)
         after_gs_text = scripted.text
         mono_gs_text = (("`{}`").format(after_gs_text))
         # TODO: emojify the :
