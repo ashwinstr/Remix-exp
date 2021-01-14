1dd7eec7ed5a15a134d3522e34e317832e8eafc8
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-14 19:02:41 +0530

    Update quotly2.py

--- userbot/modules/quotly2.py
+++ userbot/modules/quotly2.py
@@ -53,12 +53,12 @@ def progress(
             f"**Duration :** `{time_formatter(elapsed_time)}`"
         )
         if file_name:
-            await gdrive.edit(
+            gdrive.edit(
                 f"**{prog_type}**\n\n"
                 f"**File Name : **`{file_name}`**\nStatus**\n{tmp}"
             )
         else:
-            await gdrive.edit(f"**{prog_type}**\n\n" f"**Status**\n{tmp}")
+            gdrive.edit(f"**{prog_type}**\n\n" f"**Status**\n{tmp}")
 
 
 @register(outgoing=True, pattern=r"^\.q")
