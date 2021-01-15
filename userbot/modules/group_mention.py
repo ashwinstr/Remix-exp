135e43ecf6d89acbbb1bdc78d5d26da54ae67319
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-15 17:06:10 +0530

    Update group_mention.py

--- userbot/modules/group_mention.py
+++ userbot/modules/group_mention.py
@@ -1,3 +1,6 @@
+# credits: mrconfused
+# imported by AshSTR
+
 import asyncio
 from userbot.events import register
 from userbot import PM_LOGGR_BOT_API_ID
@@ -18,18 +21,25 @@ async def log_tagged_messages(event):
                 parse_mode="html",
                 link_preview=True,
             )
-        else:
-            if not (await event.get_chat()).bot:
-                await event.client.send_message(
-                    PM_LOGGR_BOT_API_ID,
-                    f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={sender.id}'> {sender.first_name}</a>\
-                            \n<b>ID : </b><code>{sender.id}</code>",
-                    parse_mode="html",
-                    link_preview=True,
+            e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
+            fwd_message = await event.client.forward_messages(
+                    e,
+                    event.message,
+                    silent=True
                 )
-                e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
-                fwd_message = await event.client.forward_messages(
-                        e,
-                        event.message,
-                        silent=True
+        else:
+            if event.is_private:
+                if not (await event.get_chat()).bot:
+                    await event.client.send_message(
+                        PM_LOGGR_BOT_API_ID,
+                        f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={sender.id}'> {sender.first_name}</a>\
+                                \n<b>ID : </b><code>{sender.id}</code>",
+                        parse_mode="html",
+                        link_preview=True,
                     )
+                    e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
+                    fwd_message = await event.client.forward_messages(
+                            e,
+                            event.message,
+                            silent=True
+                        )
